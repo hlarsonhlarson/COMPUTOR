cd66259f9c5a6a65667aa6ffc19005a2b1f274fa
18577680 <AABadalov@sberbank.ru>
2020-10-04 18:53:42 +0300

    some additions

--- Input_parser.py
+++ Input_parser.py
@@ -1,11 +1,21 @@
+import re
+
+
 def parse_input():
     input_string = input()
     input_string = input_string.lower().replace(' ', '')
-    letters_nums = list(enumerate(input_string))
     coefficient_dict = {}
 
     x_place = [x for x, y in enumerate(input_string) if y == 'x']
     equality_place = [x for x, y in enumerate(input_string) if y == '=']
+    newstr = ''.join((ch if ch in '0123456789.-e' else ' ') for ch in input_string)
+    newstr = re.split(r'(-)', newstr)
+    nums = [elem.split() for elem in newstr]
+    print(nums)
+
+
+
+
     return coefficient_dict
 
 
