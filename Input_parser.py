11ca47d67c3513131482e444bf08831abe8248e7
hlarsonhlarson <aa-badalov1@mail.ru>
2020-10-05 20:30:53 +0300

    start working with polish notation

--- Input_parser.py
+++ Input_parser.py
@@ -1,23 +1,32 @@
-import re
+
+def polish_notation(input_args):
+    op_stack = []
+    final_stack = []
+    for elem in input_args:
+        if elem.isdigit():
+            final_stack.append(float(elem))
+        elif elem.isalpha():
+            final_stack.append(elem)
+        else:
+            op_stack.append(elem)
+    return final_stack
 
 
 def parse_input():
     input_string = input()
-    input_string = input_string.lower().replace(' ', '')
+    input_string = input_string.lower()
+    input_args = input_string.split()
     coefficient_dict = {}
 
-    x_place = [x for x, y in enumerate(input_string) if y == 'x']
-    equality_place = [x for x, y in enumerate(input_string) if y == '=']
-    newstr = ''.join((ch if ch in '0123456789.-e' else ' ') for ch in input_string)
-    newstr = re.split(r'(-)', newstr)
-    # nums = [elem.split() for elem in newstr]
-    # print(nums)
-    print(newstr)
-
-
-
+    stack = polish_notation(input_args)
 
     return coefficient_dict
 
 
 parse_input()
+
+# test case 16 * X^0 + 15 + 1233 * X^1 - 24 + 41111 * X^1 - 5 * X^2 = 8 * X^3
+# 16 * X^0 + 15 + 1233 * X^1 - 24 + 41111 * X^1 - 5 * X^2 - (8 * X^3) = 0
+# use for equation
+# if elem == '=':
+#    elem = '-'
