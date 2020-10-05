3a0665df65cf251f46dba174cf22fe1bb52ecb10
hlarsonhlarson <aa-badalov1@mail.ru>
2020-10-05 21:07:57 +0300

    strange polish notation

--- Input_parser.py
+++ Input_parser.py
@@ -12,23 +12,43 @@ def prepare_input_args(input_string):
             input_args.append(')')
             equal_is_present = 1
             break
-    print(input_args)
     return input_args, equal_is_present
 
 
 def polish_notation(input_args):
     op_stack = []
     final_stack = []
+    x_is_present = 0
+    another_mistake = 0
     for elem in input_args:
-        if elem == '=':
-            break
         if elem.isdigit():
+            print(elem)
             final_stack.append(float(elem))
-        elif elem.isalpha():
-            final_stack.append(elem)
-        else:
+        elif elem == '(':
             op_stack.append(elem)
-    return final_stack
+        elif elem == ')':
+            temp = op_stack.pop()
+            while op_stack and temp != '(':
+                final_stack.append(temp)
+                temp = op_stack.pop()
+        elif elem == '+' or elem == '-':
+            while op_stack and op_stack[-1] != '(':
+                final_stack.append(op_stack.pop())
+            op_stack.append(elem)
+        elif elem == '*' or elem == '/':
+            while op_stack and op_stack[-1] != '^':
+                final_stack.append(op_stack.pop())
+            op_stack.append(elem)
+        elif elem[0] == 'x':
+            final_stack.append(float(elem))
+            x_is_present = 1
+        else:
+            another_mistake = 1
+            break
+    print(op_stack)
+    while op_stack:
+        final_stack.append(final_stack.pop())
+    return final_stack, x_is_present, another_mistake
 
 
 def parse_input():
@@ -36,8 +56,9 @@ def parse_input():
     input_args, equal_is_present = prepare_input_args(input_string)
     coefficient_dict = {}
 
-    # stack = polish_notation(input_args)
+    stack, x_is_present, another_mistake = polish_notation(input_args)
 
+    print(stack)
     return coefficient_dict
 
 
