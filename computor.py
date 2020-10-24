db8a4f286c19075d4155e628025b4d9ee3c9817f
hlarsonhlarson <aa-badalov1@mail.ru>
2020-10-24 22:39:47 +0300

    maybe it's start of parser working

--- computor.py
+++ computor.py
@@ -1,7 +1,7 @@
 from preprocessing_string import parse_input
 from Equation import Equation
 
-input_string = "x^2 - 4 * x + 5 * x^1 = -5 * x^2"
+input_string = "-1 * x^2 - 4 * x + 5 * x^1 = -5 * x^3 + 2"
 output_stack = parse_input(input_string)
 my_equation = Equation(output_stack)
 my_equation.stack_pl_mi()
