87ee36dfe43ed19cd6b3150ac307eeba8b5bb882
hlarsonhlarson <aa-badalov1@mail.ru>
2020-10-25 20:44:50 +0300

    maybe it's start of parser working

--- Equation.py
+++ Equation.py
@@ -51,5 +51,54 @@ class Equation:
         output_string = output_string[:-2]
         output_string += '= 0'
 
+    def set_abc(self):
+        if 2 in self.coefficient_dict:
+            a = self.coefficient_dict[2]
+        else:
+            a = 0
+        if 1 in self.coefficient_dict:
+            b = self.coefficient_dict[1]
+        else:
+            b = 0
+        if 0 in self.coefficient_dict:
+            c = self.coefficient_dict[0]
+        else:
+            c = 0
+        return a,b,c
+
+    def quadratic_solver(self, biggest_pow):
+        a, b, c = self.set_abc()
+        if biggest_pow == 2:
+            D = b * b - 4 * a * c
+            if D < 0:
+                print('Discriminant is negative, can\'t solve in real numbers')
+            else:
+                x1 = -b + find_root(D, 2) / 2 / a
+                x2 = -b - find_root(D, 2) / 2 / a
+                if x1 == x2:
+                    print(f'Discriminant is equal to zero. The solution is {x1}')
+                else:
+                    print(f'Discriminant is greater than zero. \nThe solutions are:\n{x1}\n{x2}')
+        elif biggest_pow == 1:
+            print(f'The solution is {-c/b}')
+        else:
+            print('There is no solution because it\'s something strange')
+
+    def mega_solver(self):
+        print('Stub')
+
+    def find_biggest_pow(self):
+        tmp = sorted(self.coefficient_dict.items(), key=lambda x: x[0], reverse=True)
+        for key, value in tmp:
+            if value:
+                return key
+        print('The solution is R')
+        exit()
+
     def solve(self):
-        print(find_root(8, 3))
+        biggest_pow = self.find_biggest_pow()
+        print(f'Polynomial degree: {biggest_pow}')
+        if biggest_pow < 3:
+            self.quadratic_solver(biggest_pow)
+        else:
+            self.mega_solver
